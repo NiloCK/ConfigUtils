8604c960ad952f0ee052cf9eee16c549cdbed3f6
Colin Kennedy <colinkennedy@hotmail.com>
2015-11-29 15:45:21 -0330

    Add warning txt file to the auto-cleaned \temp directory

--- TempClean.ps1
+++ TempClean.ps1
@@ -1,9 +1,11 @@
 # All items in C:\Temp older than three days are deleted.
 # This runs daily.
 
-Get-ChildItem C:\Temp | ForEach-Object { 
+Get-ChildItem C:\Temp | ForEach-Object {
     if ( $_.LastWriteTime -le (Get-Date).AddDays(-3) )
-    { 
+    {
         Remove-Item $_.FullName -Recurse
     }
 }
+
+"This directory is cleaned automatically, nightly. Files older than three days will be deleted. DO NOT STORE THINGS HERE!" > DoNotLeaveThingsHere.txt
