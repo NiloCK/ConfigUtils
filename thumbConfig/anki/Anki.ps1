7d98bfa2ad72e86b5e7d372d48d442f47111f1f3
nilock <colinkennedy@hotmail.com>
2017-07-21 15:11:58 -0600

    Change flags to stop overwrite of existing files

--- thumbConfig/anki/Anki.ps1
+++ thumbConfig/anki/Anki.ps1
@@ -2,12 +2,11 @@
 $ank = start-process -FilePath .\Anki\anki.exe -ArgumentList "-b .\ankidata" -Passthru
 
 # check if this is 'one of my' computers
-if (Test-Path C:\thumb\pf\anki)
-{
+if (Test-Path C:\thumb\pf\anki) {
     # if so...
     # wait until anki has closed
     wait-process -id $ank.id
 
     # then back up the data folder to the local drive
-    Copy-Item -Recurse -Force .\ankidata C:\thumb\pf\Anki\
+    Copy-Item -Recurse -ErrorAction SilentlyContinue .\ankidata C:\thumb\pf\Anki\
 }
